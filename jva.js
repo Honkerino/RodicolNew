587f5239cc468b87a6de987be572022dbd452069
Honkerino <115784123+Honkerino@users.noreply.github.com>
2023-05-30 06:05:17 +0800

    Add files via upload

---
+++ jva.js
@@ -0,0 +1,17 @@
+function changeImage()
+{
+	var img = document.getElementById('image');
+	img.src = 'pics/image2.jpg';
+}
+function alertUser() {
+	alert("Welcome to the website")
+}
+function alertUserp() {
+	alert("This is the Policies section")
+}
+function alertUserg() {
+	alert("References! ")
+}
+function Surprise() {
+	alert("I Lied")
+}
